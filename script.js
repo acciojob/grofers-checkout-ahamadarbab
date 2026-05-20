590da15cb3b74b2eb5d67742b6c701ce4bd7e47e
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-20 23:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,37 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+	// remove old total row if already present
+	const oldRow = document.getElementById("total-Row");
+	if(oldRow) {
+		oldRow.remove();
+	}
+
+	// get all price cells
+	let prices = document.querySelectorAll(".price");
+	let total = 0;
+	
+	prices.forEach((price) => {
+		total += Number(price.textContent);
+	});
+
+	// create new row
+	let totalRow = document.createElement("tr");
+	totalRow.id = "total-Row";
+	
+	// create new cell
+	let priceCell = document.createElement("td");
+
+	// set colSpan & total price in the cell
+	priceCell.colSpan = 2;
+	priceCell.textContent = `Total Price = ${total}`;
+
+	// append cell to row
+	totalRow.appendChild(priceCell);
+
+	// append row to table
+	document.getElementById("groceryTable").appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
